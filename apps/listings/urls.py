28c494e39b47e967cb2f31c8a399e7407feaf2bf
Timothy <sainaofficial67@gmail.com>
2026-04-12 11:18:35 +0300

    added base, home template with bootstrap

--- apps/listings/urls.py
+++ apps/listings/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # Define a list of URL patterns
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home'),
+    path('jobs/', views.listings, name='jobs'),
 ]
--- apps/listings/views.py
+++ apps/listings/views.py
@@ -3,6 +3,9 @@ from apps.listings.models import Job
 
 # Create your views here.
 def index(request):
+    return render(request, 'home.html')
+
+def listings(request):
     jobs = Job.objects.all()
     context = {'jobs': jobs}
     return render(request, 'listings/index.html', context)
