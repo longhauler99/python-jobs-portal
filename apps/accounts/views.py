1cf1affd5e2f2c4c7c24e185380200f8d028b18a
timothysaina <sainaofficial67@gmail.com>
2026-04-16 18:44:44 +0300

    changed registration

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -4,19 +4,37 @@ from django.shortcuts import render, redirect
 
 def signup_view(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
 
-        if User.objects.filter(username=username).exists():
-            messages.error(request, "Username already exists")
+        # Validation
+        if not all([first_name, last_name, email, password]):
+            messages.error(request, "All fields are required")
+            return redirect("signup")
+
+        if password != password2:
+            messages.error(request, "Passwords do not match")
             return redirect('signup')
 
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already exists")
+            return redirect('signup')
+
+        # Create user
         user = User.objects.create_user(
-            username=username,
+            username=email,
             email=email,
             password=password
         )
+
+        user.first_name = first_name,
+        user.last_name = last_name,
+        user.save()
+
+
         messages.success(request, "Account created successfully")
         return redirect('login')
 
