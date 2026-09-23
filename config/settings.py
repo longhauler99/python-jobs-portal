2158e32c3be74f10a0746c837730438bb1be117f
timothysaina <sainaofficial67@gmail.com>
2026-09-23 18:33:22 +0300

    parentheses

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECRET_KEY = os.getenv("SECRET_KEY", ''.join(random.choices(string.ascii_letters + string.digits, k=50)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
